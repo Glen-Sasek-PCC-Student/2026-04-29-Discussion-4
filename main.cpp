207ec2656c06c19eb2b32da98102b9937fc4b13d
Glen-Sasek-PCC-Student <glen.sasek+student@pcc.edu>
2026-04-29 22:35:33 +0000

    Add test and command history

--- main.cpp
+++ main.cpp
@@ -42,7 +42,23 @@ using namespace std;
 // Main function
 // https://en.cppreference.com/w/cpp/language/main_function.html
 int main(int argc, char* argv[]) {
-  cout << "Hello, World!" << endl;
+  string first = "";
+  string last = "";
+  string last_2_digits = "";
+  string login = "";
+
+  char x = '\0';
+
+  const int MAX_LETTERS_OF_LAST_NAME = 5;
+
+  cin >> first >> last >> x >> x >> last_2_digits;
+
+  login = last.substr(0, MAX_LETTERS_OF_LAST_NAME);
+  login += first.at(0);
+  login += last_2_digits;
+
+  cout << login << endl;
+
   return 0;
 }
 
@@ -54,6 +70,31 @@ int main(int argc, char* argv[]) {
 Program Name:
 
 Program Description:
+Create Login Name
+
+With your programming partner, write a program that creates a login name for a user, 
+given the user's 
+first name, 
+last name, and a 
+four-digit integer as input. 
+
+Output the login name, 
+which is made up of the 
+first five letters of the last name, followed by the 
+first letter of the first name, and then the 
+last two digits of the number (use the % operator). 
+
+If the last name has less than five letters, then use all letters of the last name.
+
+Hint: Use the to_string() function to convert numerical data to a string.
+// https://en.cppreference.com/cpp/string/basic_string/to_string
+
+Example:
+Input: Poppie Panther 3045
+Output: PanthP45
+
+Input: Felipe Mora 2021
+Output: MoraF21
 
 Design:
 A. INPUT
@@ -75,6 +116,10 @@ https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Ref
 
 
 SAMPLE RUNS
-Copy from assignment document.
+Poppie Panther 3045
+PanthP45
+
+Felipe Mora 2021
+MoraF21
 
 */
